17ce4f4f0daa09c18601200b64058e4fe4955a67
DongHyunKim <akainoo123@gmail.com>
2023-08-21 13:36:53 +0900

    feat : add requirements.txt

--- common/models.py
+++ common/models.py
@@ -1,3 +1,69 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self,  password, **extra_fields):
+        user = self.model(**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(password, **extra_fields)
+
+    def create_superuser(self, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    customized User
+    """
+    username = models.CharField(
+        max_length=30,
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.email
--- common/serializers.py
+++ common/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from common.models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = User
-        fields = ['id', 'username', 'password']
+class UserCreateSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def create(self, validated_data):
+        user = User.objects.create(  # User 생성
+            username=validated_data['username'],
+        )
+        user.set_password(validated_data['password'])
+
+        user.save()
+        return user
