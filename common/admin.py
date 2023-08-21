a2fa9e70a62b12784f1519d7c6a026fd6ad62332
DongHyunKim <akainoo123@gmail.com>
2023-08-21 14:22:19 +0900

    feat : admin setting

--- common/admin.py
+++ common/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from common.models import User
+
+
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'date_joined')
+
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
