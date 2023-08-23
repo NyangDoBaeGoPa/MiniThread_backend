e122800342f8a40a6b2f6a2d03c749b7dbdc4bd7
OnlyYellow <ash1823@naver.com>
2023-08-23 10:54:26 +0900

    fix/article_model

--- article/models.py
+++ article/models.py
@@ -5,3 +5,6 @@ from django.conf import settings
 class MiniThread(models.Model):
     title = models.CharField(max_length=50) # 제목 길이: 최대 50자
     content = models.TextField() # 내용 입력
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
