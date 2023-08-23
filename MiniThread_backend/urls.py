41182a94928a2a5ed5d1763526d0e4981d3e7769
OnlyYellow <ash1823@naver.com>
2023-08-23 10:17:21 +0900

    feat/url

--- MiniThread_backend/urls.py
+++ MiniThread_backend/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path("common/", include("common.urls"))
+    path("common/", include("common.urls")),
+    path("article/", include("article.urls")),
 ]
