9e68af3b0fd92095ef15499b57d8062a6b8a7ad9
OnlyYellow <ash1823@naver.com>
2023-08-23 10:52:26 +0900

    article_url_change

--- article/urls.py
+++ article/urls.py
@@ -3,8 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('article/', views.ArticleList.as_view()),
-    path('article/<int:pk>/', views.ArticleDetail.as_view()),
+    path('list/', views.ArticleList.as_view()),
+    path('list/<int:pk>/', views.ArticleDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
