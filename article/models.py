c53f55fa06cfac953139bcfafa5a6561eb61541e
OnlyYellow <ash1823@naver.com>
2023-08-23 09:09:49 +0900

    feat/article_model_setting

--- article/models.py
+++ article/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class MiniThread(models.Model):
+    title = models.CharField(max_length=50) # 제목 길이: 최대 50자
+    content = models.TextField() # 내용 입력
