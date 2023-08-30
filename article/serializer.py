bc10537de2d457b963fb2fe885f2503f2555001b
OnlyYellow <ash1823@naver.com>
2023-08-30 15:17:11 +0900

    fix/article_serializer user 값 받아오기

--- article/serializer.py
+++ article/serializer.py
@@ -2,6 +2,9 @@ from .models import MiniThread
 from rest_framework import serializers
 
 class MiniThreadSerializer(serializers.ModelSerializer):
+    # 처음 가입할 때, account_id로만 가입하기 때문에 이 값을 serializer에서 받아와야 함.
+    user = serializers.ReadOnlyField(source = 'user.account_id')
     class Meta:
         model = MiniThread
-        fields = '__all__' # title과 content 모두 직렬화
+        # 표시되는 항목 직렬화
+        fields = ['id', 'title', 'created_at', 'updated_at', 'user', 'content']
